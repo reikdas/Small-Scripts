f20cac44d2e49f96ccf1f87f051b738b74b29aca
Pratyush Das <reikdas@gmail.com>
2018-02-02 21:01:55 +0530

    Removed bleach library from tagremoval.py

--- Python/tagremoval.py
+++ Python/tagremoval.py
@@ -9,7 +9,6 @@ Created on Fri Feb  2 20:07:01 2018
 #Removes html tags from a file
 
 import os
-import bleach 
 
 path = input('Enter directory path of file : ')
 file = input('Enter name of file : ')
@@ -19,9 +18,15 @@ filename = path+file
 f = open(filename, "r")
 temporary = path+'temp.txt'
 temp = open(temporary,"w+")
+#Add tags as needed
+tags = ['<html>','</html>','<body>','</body>','<head>','</head>']
 for line in f:
-    newline = bleach.clean(line, tags=[], attributes={}, styles=[], strip=True)
-    temp.write(newline+'\n');
+    newline = ''
+    for word in line.split():
+        if word not in tags : 
+            newline = newline + word + ' '
+    if newline is not '' : 
+        temp.write(newline+'\n')
 os.remove(filename)
 os.rename(temporary,filename)
 temp.close()
